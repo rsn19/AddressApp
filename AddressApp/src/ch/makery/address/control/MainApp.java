1b46ed147b30a04c3931be56a5a17a0c951f7d3b
Miguel Halys <mhalys91@gmail.com>
2015-11-08 20:34:00 +0100

    Parte 4 tutorial

--- AddressApp/src/ch/makery/address/control/MainApp.java
+++ AddressApp/src/ch/makery/address/control/MainApp.java
@@ -10,6 +10,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -57,7 +58,8 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
-
+        this.primaryStage.getIcons().add(new Image("file:resources/images/addressbook_icon.png"));
+        
         initRootLayout();
 
         showPersonOverview();
