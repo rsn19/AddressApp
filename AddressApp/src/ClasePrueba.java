2e2331d4e708398559238fe3dc3b5f6544d34249
Miguel Halys <mhalys91@gmail.com>
2015-11-08 19:41:40 +0100

    08-11-2015 - Parte 3 tutorial hecha

--- AddressApp/src/ClasePrueba.java
+++ AddressApp/src/ClasePrueba.java
@@ -1,4 +1,18 @@
+import ch.makery.address.model.Person;
+import javafx.beans.InvalidationListener;
+import javafx.beans.Observable;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 
-public class ClasePrueba {
+public class ClasePrueba implements ChangeListener<Person>{
+
+
+	@Override
+	public void changed(ObservableValue<? extends Person> observable, Person oldValue, Person newValue) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
 
 }
