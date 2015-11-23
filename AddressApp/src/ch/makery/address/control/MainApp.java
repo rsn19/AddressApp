645fddfe93eb61b066ba12864a298a6b27b6be8d
rsn19 <daniel.rosino@yahoo.es>
2015-11-23 14:42:39 +0100

    Login hecho

--- AddressApp/src/ch/makery/address/control/MainApp.java
+++ AddressApp/src/ch/makery/address/control/MainApp.java
@@ -18,11 +18,25 @@ import ch.makery.address.view.RootLayoutController;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -30,6 +44,7 @@ public class MainApp extends Application {
 
     private Stage primaryStage;
     private BorderPane rootLayout;
+  
     
     // ... AFTER THE OTHER VARIABLES ...
 
@@ -52,6 +67,8 @@ public class MainApp extends Application {
         personData.add(new Person("Anna", "Best"));
         personData.add(new Person("Stefan", "Meier"));
         personData.add(new Person("Martin", "Mueller"));
+        
+        
     }
 
     /**
@@ -66,14 +83,71 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+    	
         this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("AddressApp");
+        //this.primaryStage.setTitle("AddressApp");
         this.primaryStage.getIcons().add(new Image("file:resources/images/addressbook_icon.png"));
         
-        initRootLayout();
+       /* initRootLayout();
 
-        showPersonOverview();
+        showPersonOverview();*/
+        
+        
+        primaryStage.setTitle("JavaFX Welcome");
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(25, 25, 25, 25));
+
+        Text scenetitle = new Text("Welcome");
+        scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        grid.add(scenetitle, 0, 0, 2, 1);
+
+        Label userName = new Label("User Name:");
+        grid.add(userName, 0, 1);
+
+        TextField userTextField = new TextField();
+        grid.add(userTextField, 1, 1);
+
+        Label pw = new Label("Password:");
+        grid.add(pw, 0, 2);
+
+        PasswordField pwBox = new PasswordField();
+        grid.add(pwBox, 1, 2);
+
+        Button btn = new Button("Sign in");
+        HBox hbBtn = new HBox(10);
+        hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn.getChildren().add(btn);
+        grid.add(hbBtn, 1, 4);
+
+        final Text actiontarget = new Text();
+        grid.add(actiontarget, 1, 6);
+
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+        	
+        	
+
+            @Override
+            public void handle(ActionEvent e) {
+                actiontarget.setFill(Color.FIREBRICK);
+                actiontarget.setText("Sign in button pressed");
+                
+                if (userTextField.getText().toString().equals("admin") && pwBox.getText().toString().equals("1234")) {
+                	 initRootLayout();
+
+                     showPersonOverview();
+                }
+            }
+        });
+
+        Scene scene = new Scene(grid, 300, 275);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
+
+    
 
     /**
      * Initializes the root layout.
